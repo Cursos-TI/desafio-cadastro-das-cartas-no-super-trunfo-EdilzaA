a6b9f4a2b76ab6180c3dbee907a08c15a87b8ecb
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-28 02:00:29 +0000

    Alteracao da quantidade de caracteres na variavel cidade

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,7 +3,7 @@
 int main(){
     
     int carta = 1;
-    char estado1 [4] ;
+    char estado1 [10] ;
     char codigo1 [10] = "F01";
     char cidade1 [50] = "Fortaleza";
     int populacao1 = 15262000;
